6ef2da6a5d606f44664b7b9d75764c1f27c6462e
Heather0k <heather@Heathers-Air.fritz.box>
2023-03-29 22:57:48 +0100

    finish date math and print results

--- components/form.tsx
+++ components/form.tsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-import Link from 'next/link'
 import {useGetTimeBetween} from "../hooks/use-get-time-diff";
 import {useState} from "react";
 import MagicIcon from "../public/icons/magic";
@@ -14,12 +13,20 @@ const FlexContainer = styled.div`
   flex-flow: column wrap;
   max-width: 800px;
   margin-top: 3rem;
-  
+
   font-weight: 500;
 `
 
 const Result = styled.div`
-    border: 2px solid pink;
+  border: 2px solid #12202d;
+  border-radius: 4px;
+  padding: 1em;
+  margin: 1em;
+  font-size: 1.2em;
+
+  svg {
+    vertical-align: middle;
+  }
 `
 
 const FormContainer = styled.form`
@@ -34,7 +41,7 @@ const FormItem = styled.div`
   flex-direction: column;
   text-align: center;
   padding: 1em;
-  
+
   & label {
     font-size: 1.2rem;
     padding: 0.5em;
@@ -45,12 +52,12 @@ const FormItem = styled.div`
         opacity: 0;
       }
     }
-    
+
     svg {
       vertical-align: middle;
       transform: rotateZ(-45deg);
-      transition: all ease 0.5s;      
-   
+      transition: all ease 0.5s;
+
     }
   }
 
@@ -89,9 +96,16 @@ export default function Form() {
     const today = new Date();
     const todayDate = today.toDateString();
 
-    const [issueValid, setIssueValid] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [passAge, setPassAge] = useState(null);
+    const [travelDiff, setTravelDiff] = useState(null);
+    const [readableAge, setReadableAge] = useState('');
+    const [readableExpiry, setReadableExpiry] = useState('');
+    const [lastDate, setLastDate] = useState('');
+
 
     const handleSubmit = async (event) => {
+        setIsLoading(true)
         event.preventDefault()
 
         const data = {
@@ -99,36 +113,67 @@ export default function Form() {
             expiry: event.target.expiry.value,
             travel: event.target.travel.value,
         }
-        const printReadable = useGetTimeBetween(data.issue, todayDate);
-        console.log(printReadable)
+        const {printReadable: readableAge, diffMonths: issueMonths} = useGetTimeBetween(data.issue, todayDate);
+        const {
+            printReadable: readableTravelDiff,
+            diffMonths: expiryMonths
+        } = useGetTimeBetween(data.travel, data.expiry);
+        const {printReadable: expiry} = useGetTimeBetween(todayDate, data.expiry);
+
+
+        const expiryDate = new Date(data.expiry);
+        expiryDate.setDate(expiryDate.getDate() - 90);
+
+
+        setPassAge(issueMonths)
+        setTravelDiff(expiryMonths)
+        setReadableAge(readableAge)
+        setReadableExpiry(expiry)
+        setLastDate(expiryDate.toDateString())
+        setIsLoading(false)
+
     }
 
     return (
         <FlexContainer>
 
-            <Result></Result>
 
             <FormContainer onSubmit={handleSubmit}>
                 <div style={{display: 'flex'}}>
-                <FormItem>
-                    <label htmlFor="issue">Issue date </label>
-                    <input type="date" id="issue" name="issue" required/>
-                </FormItem>
+                    <FormItem>
+                        <label htmlFor="issue">Issue date </label>
+                        <input type="date" id="issue" name="issue" required/>
+                    </FormItem>
 
-                <FormItem>
-                <label htmlFor="expiry">Expiry Date </label>
-                <input type="date" id="expiry" name="expiry" required/>
-                </FormItem>
+                    <FormItem>
+                        <label htmlFor="expiry">Expiry Date </label>
+                        <input type="date" id="expiry" name="expiry" required/>
+                    </FormItem>
 
-                <FormItem>
-                <label htmlFor="expiry">Travel Date <PlaneIcon/></label>
-                <input type="date" id="travel" name="travel" required/>
-                </FormItem>
+                    <FormItem>
+                        <label htmlFor="expiry">Travel Date <PlaneIcon/></label>
+                        <input type="date" id="travel" name="travel" required/>
+                    </FormItem>
                 </div>
 
                 <SubmitButton type="submit">Check your passport <MagicIcon/></SubmitButton>
 
             </FormContainer>
+
+            {!isLoading && passAge && (
+                <Result>
+                    <p> Passport is less than 10 years old ? {passAge < 120 ? <TickIcon fill={'green'}/> :
+                        <NoEntryIcon fill={'red'}/>} </p>
+                    <p> Expiry date is less than 3 months from departure ? {travelDiff > 3 ?
+                        <TickIcon fill={'green '}/> : <NoEntryIcon fill={'red'}/>} </p>
+
+                    <p> Your passport is {readableAge} old</p>
+                    <p> Your passport will expire in {readableExpiry}</p>
+
+                    <p> The last date you can travel in the EU is {lastDate} </p>
+                </Result>
+            )}
+
 
         </FlexContainer>
     )
--- hooks/use-get-time-diff.ts
+++ hooks/use-get-time-diff.ts
@@ -8,8 +8,9 @@ export function useGetTimeBetween(start: any, end: any) {
 
     // get total years / months ect
     const diffYears = Math.floor(diffDays/365);
-    const diffMonths = Math.floor(diffYears/12);
-    const diffWeeks = Math.floor(diffYears/52);
+    const diffWeeks = Math.floor(diffDays/7)
+    const diffMonths = Math.floor(diffWeeks/4);
+
 
     // get remainders to display
     // does not account for leap years or feb lol
@@ -18,28 +19,29 @@ export function useGetTimeBetween(start: any, end: any) {
     const remainderWeeks = (diffMonths % 4);
     const remainderDays = (diffDays % 7);
 
-    // todo need diff for days
     const diffs = [
         {id: 1, name: 'years', value: diffYears},
         {id: 2, name: 'months', value: diffMonths},
         {id: 3, name: 'weeks', value: diffWeeks},
         {id: 4, name: 'days', value: remainderDays},
     ]
+
     const remainders = [
         {id: 2, name: 'months', value: remainderMonths},
         {id: 3, name: 'weeks', value: remainderWeeks},
         {id: 4, name: 'days', value: remainderDays},
     ]
-
     // find the first diff with a value to start the print readable
         const diff = diffs.find(x => x.value > 0);
 
     // then only print out what comes after the diff
         const newRemArr = remainders.filter(x => x.id > diff.id && x.value > 0);
         const rem = newRemArr.map(r => {return `${r.value} ${r.name}`});
+        const remAnd = rem.slice(0, rem.length -1) + " and " + rem.slice(rem.length -1);
 
     // put it all together!
-        const printReadable = `${diff.value} ${diff.name} ${rem} ago`;
+        const printReadable = `${diff.value} ${diff.name}, ${remAnd}`;
+
 
     return {
         diffYears,
